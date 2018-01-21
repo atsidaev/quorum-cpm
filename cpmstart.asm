d6976f94754915d146e5d5232206d52777d3beec
Alexander Tsidaev <a.tsidaev@gmail.com>
2018-01-21 21:35:31 +0500

    CPM initialization code for SYS 3.0

--- cpmstart.asm
+++ cpmstart.asm
@@ -20,8 +20,9 @@ CPM_START:
 		ld	bc, PORT_CPM
 		ld	a, 84h
 		out	(c), a
-		ld	hl, BUFD1 ; some unused (yet) address
 		ld	b, 7Fh		; high of PORT_ZX128
+		push bc
+		ld	hl, 0FFFFh
 		ld	a, 1Fh		; page 7
 		out	(c), a
 		ld 	(hl), a
@@ -30,6 +31,7 @@ CPM_START:
 		ld	(hl), a
 		ld	a, 19h		; page 0
 		out	(c), a
+		ld e,(hl)	
 		ld	(hl), a
 		ld	a, 1Fh
 		out	(c), a
@@ -45,53 +47,45 @@ CPM_START:
 loc_A8B4:
 		ld      (NP_END), a
 		or      a
-		push    af
-		ld      hl, 0
-		jr      z, DONE_WITH_RAMDISK
+		jr z,_im1_set
 
 ; Fill catalog record of ramdisk
+_ramdisk_cat:
 		ld      a, 19h
 		out     (c), a
+		ld (hl),e
 		ld      hl, 0C000h
-		ld      de, 400h
-
-loc_A8C8:
-		ld      (hl), 0E5h
-		inc     hl
-		dec     de
-		ld      a, e
-		or      d
-		jr      nz, loc_A8C8
+		ld      de, 1fh
+_ramdisk_cat_l0:
+		ld a,(hl)
+		cp 0e5h
+		jr z,_ramdisk_cat_l1
+		cp 020h
+		jr nc,_ramdisk_cat_l2
+_ramdisk_cat_l1:
+		inc hl
+		ld a,(hl)
+		cp 020h
+		jr c,_ramdisk_cat_l2
+		add hl,de
+		ld a,h
+		cp 0c4h
+		jr c,_ramdisk_cat_l0
+		jr _set_page_7
+_ramdisk_cat_l2:
+		ld hl,0c000h
+		ld de,0c001h
+		ld bc,003ffh
+		ld (hl), 0E5h
+		ldir
+_set_page_7:
+		pop bc
 		ld      a, 1Fh
 		out     (c), a
-		ld      hl, 0DB60h ; some address in BIOS?
-
-DONE_WITH_RAMDISK:
-		pop     af
-		ld      (0DBAFh), hl ; another address in BIOS
-		ld      hl, 0
-		jr      z, loc_A8EB
-		inc     a
-		ld      de, 10h
-
-loc_A8E4:
-		dec     a
-		jr      z, loc_A8EA
-		add     hl, de
-		jr      loc_A8E4
-
-loc_A8EA:
-		dec     hl
-
-loc_A8EB:
-		ld	(0DBA1h), hl
-		ld	a, 0C3h ; JP
+_im1_set:
+		ld      a, 0C9h ; some address in BIOS?
 		ld	(AINTM1), a	; injecting IM1	vector opcode
-		ld	hl, 0DB33h	; injecting IM1	vector address
-		ld	(AINTM1 + 1), hl
 		im	1
-		ld	a, (0EF14h)
-		out	(0FEh),	a
 		ld	hl, LINE1
 		call	PRINT
 		ld	hl, DISK_NAME
@@ -99,14 +93,15 @@ loc_A8EB:
 		ld	hl, LINE2
 		call	PRINT
 		ld	hl, CMD_LENGTH
+		ld	de, BOOTCMD
 		ld	a, (hl)
 		or	a
-		jr	z, loc_A9E7
-		ld	de, BOOTCMD
+		jr	z, _im1_set_end
 		ld	bc, 20h
 		ldir
-
-loc_A9E7:
+		xor a
+_im1_set_end:
+		ld (de),a
 		jp	BIOS_START ; BOOT vector is the first BIOS command
 
 PRINT:
@@ -118,7 +113,7 @@ PRINT:
 		inc	hl
 		jr	PRINT
 
-		db 0, 0
+		db 0, 0, 0, 0, 0, 0, 0, 0, 0
 
 LINE1:		db 1Bh,	81h, 82h, 1Bh, 42h, 1, 0, 0 	; move cursor somewhere
 LINE2:		db 1Bh,	96h, 80h, 0, 0,	0, 0, 0  ; move cursor to (x,y=)(16,0) ?
